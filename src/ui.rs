b13c78281557dcf585118c92e2bcc46c45f0818b
Ruslan Prokopchuk <fer.obbee@gmail.com>
2018-12-17 20:24:15 +1100

    add stack manipulation words

--- src/ui.rs
+++ src/ui.rs
@@ -58,42 +58,109 @@ impl Handler {
         let mut nodes = Vec::new();
         let mut tokens = Vec::new();
         let mut g = AudioGraph::new(channels);
-        for (i, token) in text.split_whitespace().enumerate() {
-            let node: Node = match token {
-                "s" => Box::new(Osc::new(channels, sample_rate, sine)),
-                "sine" => Box::new(OscPhase::new(channels, sample_rate, sine)),
-                "t" => Box::new(Osc::new(channels, sample_rate, triangle)),
-                "tri" => Box::new(OscPhase::new(channels, sample_rate, triangle)),
-                "w" => Box::new(Phasor::new(channels, sample_rate)),
-                "saw" => Box::new(Phasor0::new(channels, sample_rate)),
-                "p" | "pulse" => Box::new(Pulse::new(channels, sample_rate)),
-                "+" => Box::new(Fn2::new(channels, add)),
-                "-" => Box::new(Fn2::new(channels, sub)),
-                "*" => Box::new(Fn2::new(channels, mul)),
-                "/" => Box::new(Fn2::new(channels, div)),
-                "unit" => Box::new(Fn1::new(channels, unit)),
-                "r" | "range" => Box::new(Fn3::new(channels, range)),
-                "n" | "noise" => Box::new(Noise::new(channels)),
-                "delay" => Box::new(Delay::new(channels, sample_rate, 60.0)),
-                "fb" | "feedback" => Box::new(Feedback::new(channels, sample_rate, 60.0)),
-                "lpf" => Box::new(LPF::new(channels, sample_rate)),
-                "hpf" => Box::new(HPF::new(channels, sample_rate)),
-                "l" | "bqlpf" => {
-                    Box::new(BiQuad::new(channels, sample_rate, make_lpf_coefficients))
-                }
-                "h" | "bqhpf" => {
-                    Box::new(BiQuad::new(channels, sample_rate, make_hpf_coefficients))
-                }
-                "m2f" | "midi2freq" => Box::new(Fn1::new(channels, midi2freq)),
-                "round" => Box::new(Fn1::new(channels, round)),
-                "sin" => Box::new(Fn1::new(channels, sin)),
-                "pan" => Box::new(Pan3::new(channels)),
-                "pan1" => Box::new(Pan1::new(channels)),
-                "pan2" => Box::new(Pan2::new(channels)),
-                "in" | "input" => Box::new(Input::new(channels)),
+        for token in text.split_whitespace() {
+            let node: Option<Node> = match token {
+                "s" => Some(Box::new(Osc::new(channels, sample_rate, sine))),
+                "sine" => Some(Box::new(OscPhase::new(channels, sample_rate, sine))),
+                "t" => Some(Box::new(Osc::new(channels, sample_rate, triangle))),
+                "tri" => Some(Box::new(OscPhase::new(channels, sample_rate, triangle))),
+                "w" => Some(Box::new(Phasor::new(channels, sample_rate))),
+                "saw" => Some(Box::new(Phasor0::new(channels, sample_rate))),
+                "p" | "pulse" => Some(Box::new(Pulse::new(channels, sample_rate))),
+                "+" => Some(Box::new(Fn2::new(channels, add))),
+                "-" => Some(Box::new(Fn2::new(channels, sub))),
+                "*" => Some(Box::new(Fn2::new(channels, mul))),
+                "/" => Some(Box::new(Fn2::new(channels, div))),
+                "unit" => Some(Box::new(Fn1::new(channels, unit))),
+                "r" | "range" => Some(Box::new(Fn3::new(channels, range))),
+                "n" | "noise" => Some(Box::new(Noise::new(channels))),
+                "delay" => Some(Box::new(Delay::new(channels, sample_rate, 60.0))),
+                "fb" | "feedback" => Some(Box::new(Feedback::new(channels, sample_rate, 60.0))),
+                "lpf" => Some(Box::new(LPF::new(channels, sample_rate))),
+                "hpf" => Some(Box::new(HPF::new(channels, sample_rate))),
+                "l" | "bqlpf" => Some(Box::new(BiQuad::new(
+                    channels,
+                    sample_rate,
+                    make_lpf_coefficients,
+                ))),
+                "h" | "bqhpf" => Some(Box::new(BiQuad::new(
+                    channels,
+                    sample_rate,
+                    make_hpf_coefficients,
+                ))),
+                "m2f" | "midi2freq" => Some(Box::new(Fn1::new(channels, midi2freq))),
+                "round" => Some(Box::new(Fn1::new(channels, round))),
+                "sin" => Some(Box::new(Fn1::new(channels, sin))),
+                "pan" => Some(Box::new(Pan3::new(channels))),
+                "pan1" => Some(Box::new(Pan1::new(channels))),
+                "pan2" => Some(Box::new(Pan2::new(channels))),
+                "in" | "input" => Some(Box::new(Input::new(channels))),
                 _ => match token.parse::<Sample>() {
-                    Ok(x) => Box::new(Constant::new(channels, x)),
-                    Err(_) => {
+                    Ok(x) => Some(Box::new(Constant::new(channels, x))),
+                    Err(_) => None,
+                },
+            };
+            nodes.push(node.and_then(|node| Some(g.add_node(node))));
+            tokens.push(token);
+        }
+        let mut stack = Vec::new();
+        for (i, (idx, token)) in nodes.into_iter().zip(&tokens).enumerate() {
+            match idx {
+                Some(idx) => {
+                    let inputs = g.node(idx).inputs();
+                    if stack.len() < (inputs as usize) {
+                        report_error(
+                            root,
+                            &format!(
+                                "Node #{} `{}` has not enough inputs on the stack.",
+                                i + 1,
+                                token
+                            ),
+                        );
+                        return;
+                    }
+                    let mut sources = Vec::new();
+                    for _ in 0..inputs {
+                        sources.push(stack.pop().unwrap());
+                    }
+                    g.set_sources_rev(idx, &sources);
+                    stack.push(idx);
+                }
+                None => match *token {
+                    "pop" => {
+                        if stack.is_empty() {
+                            report_error(root, &format!("Nothing to pop at #{}!", i + 1));
+                            return;
+                        }
+                        stack.pop();
+                    }
+                    "swap" => {
+                        let len = stack.len();
+                        if len < 2 {
+                            report_error(root, &format!("Nothing to swap at #{}!", i + 1));
+                            return;
+                        }
+                        stack.swap(len - 2, len - 1);
+                    }
+                    "dup" => match stack.last() {
+                        Some(idx) => {
+                            stack.push(*idx);
+                        }
+                        None => {
+                            report_error(root, &format!("Nothing to dup at #{}!", i + 1));
+                            return;
+                        }
+                    },
+                    "rot" => {
+                        let len = stack.len();
+                        if len < 3 {
+                            report_error(root, &format!("Nothing to rot at #{}!", i + 1));
+                            return;
+                        }
+                        stack.swap(len - 2, len - 1);
+                        stack.swap(len - 3, len - 1);
+                    }
+                    _ => {
                         report_error(
                             root,
                             &format!("Node #{} `{}` is unknown module.", i + 1, token),
@@ -101,42 +168,9 @@ impl Handler {
                         return;
                     }
                 },
-            };
-            nodes.push(g.add_node(node));
-            tokens.push(token);
-        }
-        let mut new_text = Vec::new();
-        let mut stack = Vec::new();
-        let mut widths = Vec::new();
-        let space = " ";
-        for (i, (idx, token)) in nodes.into_iter().zip(&tokens).enumerate() {
-            let inputs = g.node(idx).inputs();
-            if stack.len() < (inputs as usize) {
-                report_error(
-                    root,
-                    &format!(
-                        "Node #{} `{}` has not enough inputs on the stack.",
-                        i + 1,
-                        token
-                    ),
-                );
-                return;
             }
-            let mut sources = Vec::new();
-            let mut width = 0;
-            for _ in 0..inputs {
-                sources.push(stack.pop().unwrap());
-                width = width.max(widths.pop().unwrap());
-            }
-            g.set_sources_rev(idx, &sources);
-            stack.push(idx);
-            widths.push(width + token.len());
-            new_text.push(format!("{}{}", space.repeat(width), token));
         }
         report_error(root, "");
-        // TODO to autoformat or not to autoformat?
-        let text = new_text.join("\n");
-        // set_editor_text(root, &text);
         *self.text.lock() = text;
         *self.graph.lock() = g
     }
